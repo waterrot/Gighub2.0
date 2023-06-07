f5c75d9c7459fccf463c8821a20e42e10eeb0aba
Raymond <oudloosdrecht@gmail.com>
2023-06-07 12:24:54 +0200

    inital commit

---
+++ app/src/main/java/com/example/gighub/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.gighub.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
